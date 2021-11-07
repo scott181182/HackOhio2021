a3f3dc9964e5c236f4ce185dcd0e9a04471c2daf
Emily Wang <emilywang@Emilys-MacBook-Pro.local>
2021-11-06 21:13:24 -0400

    Added car controls

---
+++ Assets/userMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userMove : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed;
+
+    Vector3 velocity;
+
+    void Start()
+    {
+        speed = 12f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
